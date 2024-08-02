f9b6b9cc218cf0a003d093215512c8735b2a4d2b
Villy-P <valeriuspetrini@gmail.com>
2024-08-02 18:13:08 -0500

    fix: change `vite.config.js`

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [svelte()],
+	resolve: {
+		alias: {
+			"@": path.resolve("/src"),
+		},
+	},
 });
