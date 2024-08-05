2aba4a2da39ed2665c08ab8cf1af6718a2383b62
Villy-P <valeriuspetrini@gmail.com>
2024-08-04 20:09:16 -0500

    feat: add guides to component

--- src/components/data.svelte
+++ src/components/data.svelte
@@ -1,1 +1,8 @@
-<p>This is a component</p>
+<p>This is a quickstart for building your VSCode webview extension with Svelte</p>
+<p>Some files and folders for you to look out for:</p>
+<p>src/extension/extension.ts --- Your base extension file for VSCode</p>
+<p>src/extension/ --- Place all TypeScript files relating to your backend here!</p>
+<p>src/App.svelte --- This is your webview page</p>
+<p>src/components/ --- Place your components here</p>
+<p>src/app.css --- Your base CSS file</p>
+<p>You can place TypeScript files that relate to the frontend anywhere in the src directory.</p>
