d047467d07365d04f0e5db78062df98fcc5ff987
Villy-P <valeriuspetrini@gmail.com>
2025-02-04 15:30:09 -0600

    chore: update rollup plugins and refactor app mounting in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-import App from './App.svelte';
-import './css/app.css';
+import { mount } from 'svelte';
+import App from './App.svelte'
 
-const app = new App({
-	target: document.body,
-});
+const app = mount(App, { target: document.getElementById("app")! });
 
 export default app;
