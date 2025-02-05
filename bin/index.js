0d94dc1e4fb357ebc3fd90d7e172ef122a0f5a70
Villy-P <valeriuspetrini@gmail.com>
2025-02-05 09:05:38 -0600

    feat: refactor directory check to use async/await and improve user prompt

--- bin/index.js
+++ bin/index.js
@@ -7,29 +7,41 @@ const term = require('terminal-kit').terminal;
 const projectName = process.argv[2] || 'my-project';
 const projectPath = path.resolve(projectName);
 
-function checkEmptyDirectory() {
-    if (fs.readdirSync(projectPath).length > 0) {
-        term.yellow("WARNING: Directory is not empty. Continue? [Y|n] ");
-        term.yesOrNo({ yes: ['y', 'ENTER'], no: ['n'] }, function (_error, result) {
-            if (result) {
+function promptYesOrNo(question) {
+    return new Promise((resolve) => {
+        term.yellow(question);
+        term.yesOrNo({ yes: ['y', 'ENTER'], no: ['n'] }, function (error, result) {
+            resolve(result);
+        });
+    });
+}
+
+async function checkEmptyDirectory() {
+    return new Promise(async (resolve) => {
+        if (fs.readdirSync(projectPath).length > 0) {
+            const shouldContinue = await promptYesOrNo("WARNING: Directory is not empty. Continue? [Y|n] ");
+            if (shouldContinue) {
                 term("\n");
-                runProject();
+                resolve();
             } else {
                 term.red("Exiting...\n");
                 process.exit();
             }
-        });
-    }
+        }
+    });
 }
 
-function runProject() {
+async function runProject() {
     if (!process.argv[2])
         term.yellow("WARNING: No project name provided. Using 'my-project' as project name.\n");
     
     if (!fs.existsSync(projectPath))
         fs.mkdirSync(projectPath);
 
-    checkEmptyDirectory();
+    await checkEmptyDirectory();
+
+    console.log("FINISHED");
+    process.exit();
 }
 
 runProject();
