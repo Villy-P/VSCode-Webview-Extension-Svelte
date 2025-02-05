4a830ee9aca4d23ca9f1786acb711b77f49fe27b
Villy-P <valeriuspetrini@gmail.com>
2025-02-05 13:48:49 -0600

    feat: migrate project initialization to use ES modules and inquirer for prompts

--- bin/index.js
+++ bin/index.js
@@ -1,63 +1,37 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-// const term = require('terminal-kit').terminal;
+import { existsSync, mkdirSync, readdirSync } from 'fs';
+import { resolve } from 'path';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import { confirm, input } from '@inquirer/prompts';
 
-// const projectName = process.argv[2] || 'my-project';
-// const projectPath = path.resolve(projectName);
-// let projectDescription = '';
-// let projectDisplayName = '';
+let projectRelativePath = process.argv[2];
+let projectPath = '';
+let projectName = '';
+let projectDescription = '';
+let projectDisplayName = '';
 
-// function promptYesOrNo(question) {
-//     return new Promise((resolve) => {
-//         term.yellow(question);
-//         term.yesOrNo({ yes: ['y', 'ENTER'], no: ['n'] }, function (error, result) {
-//             resolve(result);
-//         });
-//     });
-// }
+async function runProject() {
+    if (!process.argv[2])
+        projectRelativePath = await input({ 
+            message: chalk.blue('Enter project relative path. Leave blank to use current directory') ,
+            default: "."
+        });
+    projectPath = resolve(process.cwd(), projectRelativePath);
 
-// async function checkEmptyDirectory() {
-//     return new Promise(async (resolve) => {
-//         if (fs.readdirSync(projectPath).length > 0) {
-//             const shouldContinue = await promptYesOrNo("WARNING: Directory is not empty. Continue? [Y|n] ");
-//             if (shouldContinue) {
-//                 term("\n");
-//                 resolve();
-//             } else {
-//                 term.red("Exiting...\n");
-//                 process.exit();
-//             }
-//         } else {
-//             resolve();
-//         }
-//     });
-// }
+    if (!existsSync(projectPath))
+        mkdirSync(projectPath);
 
-// async function getProjectMetadata() {
-//     term.green("\nFirst, let's set up some metadata for your project.\n\n");
-//     return new Promise(async (resolve) => {
-//         term.blue("Enter a description: ");
-//         projectDescription = await term.inputField({}).promise;
-//         term.blue("\nEnter a display name: ");
-//         projectDisplayName = await term.inputField({}).promise;
-//         resolve();
-//     });
-// }
+    if (readdirSync(projectPath).length > 0) {
+        const answer = await confirm({ message: chalk.yellow('WARNING: Directory is not empty. Continue?') });
+        if (answer) {
+            console.log("\n");
+        } else {
+            console.log(chalk.red("Exiting...\n"));
+            process.exit();
+        }
+    }
+}
 
-// async function runProject() {
-//     if (!process.argv[2])
-//         term.yellow("WARNING: No project name provided. Using 'my-project' as project name.\n");
-    
-//     if (!fs.existsSync(projectPath))
-//         fs.mkdirSync(projectPath);
-
-//     await checkEmptyDirectory();
-//     await getProjectMetadata();
-
-//     console.log("FINISHED");
-//     process.exit();
-// }
-
-// runProject();
+runProject();
