fc715c884262466bd5ab1eeab60bf49874feb035
Villy-P <valeriuspetrini@gmail.com>
2025-02-05 08:31:19 -0600

    feat: enhance index.js to accept project name and add terminal-kit dependency

--- bin/index.js
+++ bin/index.js
@@ -2,5 +2,12 @@
 
 const fs = require('fs');
 const path = require('path');
+const term = require('terminal-kit').terminal;
 
-console.log("Hello World");
+const projectName = process.argv[2] || 'my-project';
+const projectPath = path.resolve(projectName);
+
+if (!process.argv[2])
+    term.yellow("WARNING: No project name provided. Using 'my-project' as project name.\n");
+
+console.log(projectName, projectPath);
