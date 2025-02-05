b43a4c011f8eb1df9adb9449d2de65c4fc639fe1
Villy-P <valeriuspetrini@gmail.com>
2025-02-05 14:37:39 -0600

    chore: update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { existsSync, mkdirSync, readdirSync } from 'fs';
+import { existsSync, fstat, mkdirSync, readdirSync, readFileSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
@@ -17,6 +17,15 @@ let projectAdditions = [];
 
 const validCategories = ['Programming Languages', 'Snippets', 'Linters', 'Themes', 'Debuggers', 'Formatters', 'Keymaps', 'SCM Providers', 'Other', 'Extension Packs', 'Language Packs', 'Data Science', 'Machine Learning', 'Visualization', 'Notebooks', 'Education', 'Testing'];
 const validAdditions = ["Tailwind"];
+
+function createFileName(path, writeFileName) {
+    const data = readFileSync(path, 'utf8');
+    writeFileSync(writeFileName, data, 'utf-8');
+}
+
+function createFileNameWithData(writeFileName, data) {
+    writeFileSync(writeFileName, data, 'utf-8');
+}
 
 async function runProject() {
     if (!process.argv[2])
@@ -61,6 +70,12 @@ async function runProject() {
         message: chalk.blue('Enter additional toolkits (optional)'), 
         choices: validAdditions
     });
+
+    createFileName("../tsconfig.node.json", `${projectPath}/tsconfig.node.json`);
+    createFileName("../tsconfig.json", `${projectPath}/tsconfig.json`);
+    createFileName("../rollup.config.mjs", `${projectPath}/rollup.config.mjs`);
+    createFileName("../postcss.config.js", `${projectPath}/postcss.config.js`);
+    createFileNameWithData(`${projectPath}/README.md`, `# ${projectName}\n\n${projectDescription}\n## Installation\n\n## Usage\n\n## License\n\n## Contributing\n\n## Changelog\n\n## Authors\n\n## Acknowledgements\n\n## Keywords\n\n${projectName} ${projectKeywords.join(" ")}`);
 }
 
 runProject();
