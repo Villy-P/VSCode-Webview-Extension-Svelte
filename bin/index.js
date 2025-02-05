c0fcb21ff26cba3d85fc42ea95450f5ecef6aa77
Villy-P <valeriuspetrini@gmail.com>
2025-02-05 08:44:27 -0600

    feat: add directory check and confirmation before project creation

--- bin/index.js
+++ bin/index.js
@@ -7,10 +7,29 @@ const term = require('terminal-kit').terminal;
 const projectName = process.argv[2] || 'my-project';
 const projectPath = path.resolve(projectName);
 
-if (!process.argv[2])
-    term.yellow("WARNING: No project name provided. Using 'my-project' as project name.\n");
+function checkEmptyDirectory() {
+    if (fs.readdirSync(projectPath).length > 0) {
+        term.yellow("WARNING: Directory is not empty. Continue? [Y|n] ");
+        term.yesOrNo({ yes: ['y', 'ENTER'], no: ['n'] }, function (_error, result) {
+            if (result) {
+                term("\n");
+                runProject();
+            } else {
+                term.red("Exiting...\n");
+                process.exit();
+            }
+        });
+    }
+}
 
-if (!fs.existsSync(projectPath))
-    fs.mkdirSync(projectPath);
+function runProject() {
+    if (!process.argv[2])
+        term.yellow("WARNING: No project name provided. Using 'my-project' as project name.\n");
+    
+    if (!fs.existsSync(projectPath))
+        fs.mkdirSync(projectPath);
 
-console.log(projectName, projectPath);
+    checkEmptyDirectory();
+}
+
+runProject();
